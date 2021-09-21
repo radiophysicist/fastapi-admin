970942f8b3613b7bb7f01bde5bf33eb927b60bf7
Denis Shatov <dshatov@gmail.com>
2021-09-21 13:21:09 +0700

    Disable using select_for_update() when updating record (due to tortoise bug)

--- fastapi_admin/routes/resources.py
+++ fastapi_admin/routes/resources.py
@@ -95,7 +95,7 @@ async def update(
             obj = (
                 await model.filter(pk=pk)
                 .using_db(conn)
-                .select_for_update()
+                # .select_for_update()
                 .get()
                 .prefetch_related(*model_resource.get_m2m_field())
             )
